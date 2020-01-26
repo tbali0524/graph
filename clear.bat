6d10218d08b06c65b72974d360b68e11cb06f427
Balint Toth <51286559+tbali0524@users.noreply.github.com>
2020-01-26 22:26:50 +0100

    Add files via upload

---
+++ clear.bat
@@ -0,0 +1,1 @@
+del output_*.txt
---
+++ test.bat
@@ -0,0 +1,6 @@
+php graph.php <test-graph_01.txt >output_01.txt 2>&1
+php graph.php <test-graph_02.txt >output_02.txt 2>&1
+php graph.php <test-graph_03.txt >output_03.txt 2>&1
+php graph.php <test-graph_04.txt >output_04.txt 2>&1
+php graph.php <test-graph_b2t03.txt >output_b2t03.txt 2>&1
+php graph.php <test-graph_b2t04.txt >output_b2t04.txt 2>&1
